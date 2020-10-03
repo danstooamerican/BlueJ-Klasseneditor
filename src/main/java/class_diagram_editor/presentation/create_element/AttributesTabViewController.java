b1e5e850921f9dacdaa8a83e2c095b44f77b611e
Daniel Augustin <daniel.augustin1@gmx.de>
2020-10-03 23:18:38 +0200

    Refactor attributes tab into separate view

---
+++ src/main/java/class_diagram_editor/presentation/create_element/AttributesTabViewController.java
@@ -0,0 +1,99 @@
+package class_diagram_editor.presentation.create_element;
+
+import class_diagram_editor.diagram.AttributeModel;
+import class_diagram_editor.diagram.Visibility;
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.SimpleBooleanProperty;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ListView;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
+
+public class AttributesTabViewController {
+
+    @FXML private ListView<AttributeModel> lstAttributes;
+
+    @FXML private ToggleGroup attributeVisibility;
+    @FXML private RadioButton rbnAttributePrivate;
+    @FXML private RadioButton rbnAttributePublic;
+    @FXML private RadioButton rbnAttributePackage;
+    @FXML private RadioButton rbnAttributeProtected;
+
+    @FXML private CheckBox ckbAttributeStatic;
+    @FXML private CheckBox ckbAttributeFinal;
+
+    @FXML private TextField txbAttributeName;
+    @FXML private TextField txbAttributeType;
+
+    @FXML private CheckBox ckbAttributeGetter;
+    @FXML private CheckBox ckbAttributeSetter;
+
+    @FXML private Button btnAddAttribute;
+    @FXML private Button btnEditAttribute;
+
+    private final BooleanProperty attributeAlreadyExists = new SimpleBooleanProperty();
+
+    public void initialize(CreateElementViewModel viewModel) {
+        lstAttributes.setCellFactory(attributeModelListView -> new DeletableAttributeListCell((attibuteModel) -> {
+            viewModel.deleteAttribute(attibuteModel);
+
+            return null;
+        }));
+        lstAttributes.itemsProperty().bindBidirectional(viewModel.attributesProperty());
+        lstAttributes.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
+            viewModel.selectAttribute(newValue);
+
+            switch (newValue.getVisibility()) {
+                case PRIVATE:
+                    attributeVisibility.selectToggle(rbnAttributePrivate);
+                    break;
+                case PUBLIC:
+                    attributeVisibility.selectToggle(rbnAttributePublic);
+                    break;
+                case PACKAGE_PRIVATE:
+                    attributeVisibility.selectToggle(rbnAttributePackage);
+                    break;
+                case PROTECTED:
+                    attributeVisibility.selectToggle(rbnAttributeProtected);
+                    break;
+            }
+        });
+
+        rbnAttributePrivate.setUserData(Visibility.PRIVATE);
+        rbnAttributePublic.setUserData(Visibility.PUBLIC);
+        rbnAttributePackage.setUserData(Visibility.PACKAGE_PRIVATE);
+        rbnAttributeProtected.setUserData(Visibility.PROTECTED);
+
+        attributeVisibility.selectedToggleProperty().addListener((observable, oldValue, newValue) -> {
+            viewModel.setAttributeVisiblity((Visibility) newValue.getUserData());
+        });
+
+        ckbAttributeFinal.selectedProperty().bindBidirectional(viewModel.attributeFinalProperty());
+        ckbAttributeStatic.selectedProperty().bindBidirectional(viewModel.attributeStaticProperty());
+        txbAttributeName.textProperty().bindBidirectional(viewModel.attributeNameProperty());
+        txbAttributeType.textProperty().bindBidirectional(viewModel.attributeTypeProperty());
+        ckbAttributeGetter.selectedProperty().bindBidirectional(viewModel.attributeGetterProperty());
+        ckbAttributeSetter.selectedProperty().bindBidirectional(viewModel.attributeSetterProperty());
+
+        btnAddAttribute.disableProperty().bind(viewModel.attributeNameProperty().isEmpty().or(viewModel.attributeTypeProperty().isEmpty()).or(attributeAlreadyExists));
+        btnEditAttribute.disableProperty().bind(viewModel.attributeNameProperty().isEmpty().or(viewModel.attributeTypeProperty().isEmpty()));
+
+        btnEditAttribute.visibleProperty().bind(lstAttributes.getSelectionModel().selectedIndexProperty().greaterThan(-1));
+
+        txbAttributeName.textProperty().addListener((observable, oldValue, newValue) -> {
+            attributeAlreadyExists.set(viewModel.attributeExists(newValue));
+        });
+
+        btnAddAttribute.setOnAction(event -> {
+            viewModel.addAttribute();
+        });
+
+        btnEditAttribute.setOnAction(event -> {
+            viewModel.editAttribute(lstAttributes.getSelectionModel().getSelectedItem());
+            lstAttributes.refresh();
+        });
+    }
+}
--- src/main/java/class_diagram_editor/presentation/create_element/CreateElementView.java
+++ src/main/java/class_diagram_editor/presentation/create_element/CreateElementView.java
@@ -1,16 +1,11 @@
 package class_diagram_editor.presentation.create_element;
 
-import class_diagram_editor.diagram.AttributeModel;
 import class_diagram_editor.diagram.Connectable;
 import class_diagram_editor.diagram.InterfaceModel;
-import class_diagram_editor.diagram.Visibility;
-import class_diagram_editor.presentation.MainScreenView;
 import de.saxsys.mvvmfx.FluentViewLoader;
 import de.saxsys.mvvmfx.FxmlView;
 import de.saxsys.mvvmfx.InjectViewModel;
 import de.saxsys.mvvmfx.ViewTuple;
-import javafx.beans.property.BooleanProperty;
-import javafx.beans.property.SimpleBooleanProperty;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
@@ -21,7 +16,6 @@ import javafx.scene.control.CheckBox;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
-import javafx.scene.control.RadioButton;
 import javafx.scene.control.Separator;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
@@ -61,106 +55,32 @@ public class CreateElementView implements FxmlView<CreateElementViewModel>, Init
 
     private final Stage stage;
 
-    private final BooleanProperty attributeAlreadyExists = new SimpleBooleanProperty();
-
     @InjectViewModel
     private CreateElementViewModel viewModel;
 
     @FXML
-    private Button btnCreateElement;
+    private AttributesTabViewController attributesTabViewController;
 
-    @FXML
-    private Button btnEditElement;
+    @FXML private Button btnCreateElement;
+    @FXML private Button btnEditElement;
+    @FXML private Button btnCancel;
 
-    @FXML
-    private Button btnCancel;
+    @FXML private ToggleButton tgbClass;
+    @FXML private ToggleButton tgbInterface;
+    @FXML private ToggleGroup elementType;
+    @FXML private TextField txbElementName;
+    @FXML private ComboBox<Connectable> cbbElementExtends;
+    @FXML private CheckBox ckbElementAbstract;
+    @FXML private ListView<InterfaceModel> lstImplements;
+    @FXML private ComboBox<InterfaceModel> cbbElementImplements;
+    @FXML private Button btnAddImplements;
+    @FXML private Label lblImplementsTitle;
+    @FXML private HBox pnlElementExtends;
+    @FXML private HBox pnlElementAbstract;
+    @FXML private Separator sprElementAbstract;
 
-    @FXML
-    private ToggleButton tgbClass;
-
-    @FXML
-    private ToggleButton tgbInterface;
-
-    @FXML
-    private ToggleGroup elementType;
-
-    @FXML
-    private TextField txbElementName;
-
-    @FXML
-    private ComboBox<Connectable> cbbElementExtends;
-
-    @FXML
-    private CheckBox ckbElementAbstract;
-
-    @FXML
-    private ListView<InterfaceModel> lstImplements;
-
-    @FXML
-    private ComboBox<InterfaceModel> cbbElementImplements;
-
-    @FXML
-    private Button btnAddImplements;
-
-    @FXML
-    private Label lblImplementsTitle;
-
-    @FXML
-    private HBox pnlElementExtends;
-
-    @FXML
-    private HBox pnlElementAbstract;
-
-    @FXML
-    private Separator sprElementAbstract;
-
-    @FXML
-    private TabPane tabPane;
-
-    @FXML
-    private Tab tabAttributes;
-
-    @FXML
-    private ListView<AttributeModel> lstAttributes;
-
-    @FXML
-    private ToggleGroup attributeVisibility;
-
-    @FXML
-    private RadioButton rbnAttributePrivate;
-
-    @FXML
-    private RadioButton rbnAttributePublic;
-
-    @FXML
-    private RadioButton rbnAttributePackage;
-
-    @FXML
-    private RadioButton rbnAttributeProtected;
-
-    @FXML
-    private CheckBox ckbAttributeStatic;
-
-    @FXML
-    private CheckBox ckbAttributeFinal;
-
-    @FXML
-    private TextField txbAttributeName;
-
-    @FXML
-    private TextField txbAttributeType;
-
-    @FXML
-    private CheckBox ckbAttributeGetter;
-
-    @FXML
-    private CheckBox ckbAttributeSetter;
-
-    @FXML
-    private Button btnAddAttribute;
-
-    @FXML
-    private Button btnEditAttribute;
+    @FXML private TabPane tabPane;
+    @FXML private Tab tabAttributes;
 
     public CreateElementView(Stage stage) {
         this.stage = stage;
@@ -168,6 +88,8 @@ public class CreateElementView implements FxmlView<CreateElementViewModel>, Init
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        attributesTabViewController.initialize(viewModel);
+
         if (viewModel.isEditMode()) {
             btnCreateElement.setVisible(false);
             btnCreateElement.setManaged(false);
@@ -266,65 +188,6 @@ public class CreateElementView implements FxmlView<CreateElementViewModel>, Init
         btnEditElement.setOnAction(event -> {
             viewModel.editElement();
             stage.close();
-        });
-
-        lstAttributes.setCellFactory(attributeModelListView -> new DeletableAttributeListCell((attibuteModel) -> {
-            viewModel.deleteAttribute(attibuteModel);
-
-            return null;
-        }));
-        lstAttributes.itemsProperty().bindBidirectional(viewModel.attributesProperty());
-        lstAttributes.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
-            viewModel.selectAttribute(newValue);
-
-            switch (newValue.getVisibility()) {
-                case PRIVATE:
-                    attributeVisibility.selectToggle(rbnAttributePrivate);
-                    break;
-                case PUBLIC:
-                    attributeVisibility.selectToggle(rbnAttributePublic);
-                    break;
-                case PACKAGE_PRIVATE:
-                    attributeVisibility.selectToggle(rbnAttributePackage);
-                    break;
-                case PROTECTED:
-                    attributeVisibility.selectToggle(rbnAttributeProtected);
-                    break;
-            }
-        });
-
-        rbnAttributePrivate.setUserData(Visibility.PRIVATE);
-        rbnAttributePublic.setUserData(Visibility.PUBLIC);
-        rbnAttributePackage.setUserData(Visibility.PACKAGE_PRIVATE);
-        rbnAttributeProtected.setUserData(Visibility.PROTECTED);
-
-        attributeVisibility.selectedToggleProperty().addListener((observable, oldValue, newValue) -> {
-            viewModel.setAttributeVisiblity((Visibility) newValue.getUserData());
-        });
-
-        ckbAttributeFinal.selectedProperty().bindBidirectional(viewModel.attributeFinalProperty());
-        ckbAttributeStatic.selectedProperty().bindBidirectional(viewModel.attributeStaticProperty());
-        txbAttributeName.textProperty().bindBidirectional(viewModel.attributeNameProperty());
-        txbAttributeType.textProperty().bindBidirectional(viewModel.attributeTypeProperty());
-        ckbAttributeGetter.selectedProperty().bindBidirectional(viewModel.attributeGetterProperty());
-        ckbAttributeSetter.selectedProperty().bindBidirectional(viewModel.attributeSetterProperty());
-
-        btnAddAttribute.disableProperty().bind(viewModel.attributeNameProperty().isEmpty().or(viewModel.attributeTypeProperty().isEmpty()).or(attributeAlreadyExists));
-        btnEditAttribute.disableProperty().bind(viewModel.attributeNameProperty().isEmpty().or(viewModel.attributeTypeProperty().isEmpty()));
-
-        btnEditAttribute.visibleProperty().bind(lstAttributes.getSelectionModel().selectedIndexProperty().greaterThan(-1));
-
-        txbAttributeName.textProperty().addListener((observable, oldValue, newValue) -> {
-            attributeAlreadyExists.set(viewModel.attributeExists(newValue));
-        });
-
-        btnAddAttribute.setOnAction(event -> {
-            viewModel.addAttribute();
-        });
-
-        btnEditAttribute.setOnAction(event -> {
-            viewModel.editAttribute(lstAttributes.getSelectionModel().getSelectedItem());
-            lstAttributes.refresh();
         });
     }
 }
