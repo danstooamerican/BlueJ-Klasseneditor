a1d484c1e063bf90de3b2549fb4a24d58c74b362
Daniel Augustin <daniel.augustin1@gmx.de>
2020-10-03 18:25:33 +0200

    Add better syntax checks when creating connections by hand

--- src/main/java/class_diagram_editor/presentation/validator/UMLConnectorValidator.java
+++ src/main/java/class_diagram_editor/presentation/validator/UMLConnectorValidator.java
@@ -1,5 +1,9 @@
 package class_diagram_editor.presentation.validator;
 
+import class_diagram_editor.diagram.ClassDiagram;
+import class_diagram_editor.diagram.ClassModel;
+import class_diagram_editor.diagram.Connectable;
+import class_diagram_editor.diagram.InterfaceModel;
 import class_diagram_editor.presentation.skins.AssociationConnectionSkin;
 import class_diagram_editor.presentation.skins.ClassSkin;
 import class_diagram_editor.presentation.skins.ExtendsConnectionSkin;
@@ -7,13 +11,17 @@ import class_diagram_editor.presentation.skins.ImplementsConnectionSkin;
 import class_diagram_editor.presentation.skins.InterfaceSkin;
 import de.tesis.dynaware.grapheditor.GConnectorValidator;
 import de.tesis.dynaware.grapheditor.model.GConnector;
+import de.tesis.dynaware.grapheditor.model.GNode;
 import javafx.beans.property.BooleanProperty;
 
 public class UMLConnectorValidator implements GConnectorValidator {
 
     private boolean drawAssociation;
+    private ClassDiagram classDiagram;
 
-    public UMLConnectorValidator(BooleanProperty drawAssociation) {
+    public UMLConnectorValidator(BooleanProperty drawAssociation, ClassDiagram classDiagram) {
+        this.classDiagram = classDiagram;
+
         drawAssociation.addListener((observable, oldValue, newValue) -> {
             this.drawAssociation = newValue;
         });
@@ -30,7 +38,40 @@ public class UMLConnectorValidator implements GConnectorValidator {
 
     @Override
     public boolean validate(GConnector source, GConnector target) {
-        return !source.getParent().equals(target.getParent());
+        GNode sourceNode = source.getParent();
+        GNode targetNode = target.getParent();
+
+        if (sourceNode.equals(targetNode)) {
+            return false;
+        }
+
+        Connectable sourceConnectable = classDiagram.findElement(sourceNode.getId());
+        Connectable targetConnectable = classDiagram.findElement(targetNode.getId());
+
+        if (sourceConnectable == null || targetConnectable == null) {
+            return false;
+        }
+
+        // two extends to the same node
+        if (sourceConnectable.getExtendsRelations().contains(targetConnectable)) {
+            return false;
+        }
+
+        // cyclic extension
+        if (targetConnectable.getExtendsRelations().contains(sourceConnectable)) {
+            return false;
+        }
+
+        if (sourceConnectable instanceof ClassModel && targetConnectable instanceof InterfaceModel) {
+            ClassModel sourceClass = (ClassModel) sourceConnectable;
+
+            // two implements to the same node
+            if (sourceClass.getImplementsInterfaces().contains(targetConnectable)) {
+                return false;
+            }
+        }
+
+        return true;
     }
 
     @Override
